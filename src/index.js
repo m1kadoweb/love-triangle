846228251dad1d6e860401bceabb766f0366d8f9
m1kadoweb <m1kadosgs@gmail.com>
2019-07-06 14:43:37 +0500

    100%

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var counter = 0;
+  var lover1 = 0;
+  var lover2 = 0;
+  var lover3 = 0;
+
+  for(var i = 0; i < preferences.length; i++) {
+  		lover1 = preferences[i];
+  		lover2 = preferences[lover1-1];
+  		lover3 = preferences[lover2-1];
+  		if(lover3 == i + 1)
+  			counter++;
+
+}
+  		return Math.floor(counter/3);
 };
+
